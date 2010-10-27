1b8a704f33dbcf4fc60f7f5532e49f60eab71dff
Byrne <byrne@majordojo.com>
2010-10-27 16:35:44 -0700

    moving spamlookup out of melody

---
+++ plugins/spamlookup/lib/spamlookup/L10N.pm
@@ -0,0 +1,13 @@
+# SpamLookup plugin for Movable Type
+# Author: Brad Choate, Six Apart (http://www.sixapart.com)
+# Released under the Artistic License
+#
+# $Id: L10N.pm 63 2006-06-26 22:16:41Z jallen $
+
+package spamlookup::L10N;
+use strict;
+
+use MT::Plugin::L10N;
+@spamlookup::L10N::ISA = qw(MT::Plugin::L10N);
+
+1;
